d2f674e2b45117c2589fc62b055d2e75aa7f6804
zhck <zhck@mail.ru>
2012-12-01 13:42:29 +0200

    Adding tests

--- config/application.rb
+++ config/application.rb
@@ -1,12 +1,14 @@
 require File.expand_path('../boot', __FILE__)
 
 # Pick the frameworks you want:
-require "active_record/railtie"
-require "action_controller/railtie"
-require "action_mailer/railtie"
-require "active_resource/railtie"
-require "sprockets/railtie"
+#require "active_record/railtie"
+#require "action_controller/railtie"
+#require "action_mailer/railtie"
+#require "active_resource/railtie"
+#require "sprockets/railtie"
 # require "rails/test_unit/railtie"
+
+require 'rails/all'
 
 if defined?(Bundler)
   # If you precompile assets before deploying to production, use this line
@@ -64,7 +66,5 @@ module Depot
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
-    
-    # config.assets.paths << Rails.root.join("app", "assets", "flashs")
   end
 end
